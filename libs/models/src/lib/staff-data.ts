e12c57d8e532c4cab343711c77888302568e2fd3
Craig Shearer <craig@supacrash.com>
2022-06-24 15:09:19 +1200

    Define staff model

---
+++ libs/models/src/lib/staff-data.ts
@@ -0,0 +1,21 @@
+import { StaffModel } from './staff-model';
+
+export const StaffData: StaffModel[] = [
+	{
+		staffCode: 'S001',
+		firstName: 'John',
+		surname: 'Smith',
+		initials: 'JS',
+		dateOfBirth: new Date('1980-01-01'),
+		accessOwnRecordOnly: true,
+		emailAddress: 'john.smith.8903@email.com',
+		addressLine1: '123 Main Street',
+		addressLine2: '',
+		addressLine3: '',
+		addressLine4: '',
+		postcode: 'AB1 2CD',
+		phoneNumber: '0123456789',
+		titleId: '1',
+		genderId: '1',
+	},
+];
---
+++ libs/models/src/lib/staff-model.ts
@@ -0,0 +1,17 @@
+export type StaffModel = {
+	staffCode: string;
+	firstName: string;
+	surname: string;
+	initials: string;
+	dateOfBirth: Date;
+	genderId: string;
+	titleId: string;
+	addressLine1: string;
+	addressLine2: string;
+	addressLine3: string;
+	addressLine4: string;
+	postcode: string;
+	accessOwnRecordOnly: boolean;
+	emailAddress: string;
+	phoneNumber: string;
+};
